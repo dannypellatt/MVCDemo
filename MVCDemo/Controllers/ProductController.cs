0ff1a4922ec4f1547990a66135aeda51bbaef75b
dannypellatt <danielpellatt92@gmail.com>
2022-08-15 14:13:35 -0400

    user can now create product

--- MVCDemo/Controllers/ProductController.cs
+++ MVCDemo/Controllers/ProductController.cs
@@ -48,6 +48,18 @@ namespace MVCDemo.Controllers
 
             return RedirectToAction("ViewProduct", new { id = product.ProductID });
         }
+
+        public IActionResult InsertProduct()
+        {
+            var prod = repo.AssignCategory();
+            return View(prod);
+        }
+
+        public IActionResult InsertProductToDatabase(Product productToInsert)
+        {
+            repo.InsertProduct(productToInsert);
+            return RedirectToAction("Index");
+        }
     }
 }
 
